2acca07bb05a727209bbcdb2d854bd1651ea2d93
afer9318 <Aruna.Fernando@sysco.com>
2024-02-24 15:52:52 +0530

    env file

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-const Pool = require('pg').Pool;
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "node_restful_api",
-    password: "postgres",
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 module.exports = pool;
